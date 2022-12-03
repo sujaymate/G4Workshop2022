ac6dde7afdc1b4be4dc886f387ba017d8d60a867
Sujay Mate <sujay.mate@gmail.com>
2022-12-03 16:42:44 +0530

    Modify DPG gen code

--- HEAP2/python/plot_dph.py
+++ HEAP2/python/plot_dph.py
@@ -35,6 +35,7 @@ eventID, index, counts = np.unique(events[:, 0], return_index=True, return_count
 
 # get single pixel events
 single_events = events[index[counts == 1]]
+emax = single_events[:, 2].max()
 
 # get pix X and Y index
 pixX = np.floor(single_events[:, 1] // 16)
@@ -42,6 +43,13 @@ pixY = np.floor(single_events[:, 1] % 16)
 
 # get the histgram and plot
 DPH, X, Y = np.histogram2d(pixX, pixY, np.arange(-0.5, 15.6, 1))
-
-plt.imshow(DPH, origin="lower", cmap="plasma")
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+im = ax.imshow(DPH, origin="lower", cmap="plasma")
+ax.set_xticks(np.arange(0, 16, 1))
+ax.set_yticks(np.arange(0, 16, 1))
+ax.set_xlabel("Pix X", fontsize=13)
+ax.set_ylabel("Pix Y", fontsize=13)
+plt.colorbar(im)
+fig.suptitle("Input energy: {0: 3.0f} keV".format(emax), fontsize=15)
+fig.savefig(rootfname.stem + "_DPH_.png", dpi=150)
 plt.show()
